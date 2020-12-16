c9a832cef0349d8f35ea9be3488d96dccfb12487
Josef212 <jcasanovasv@hotmail.com>
2020-12-16 21:25:14 +0100

    Minor updates

--- keymap.c
+++ keymap.c
@@ -60,37 +60,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
     [L_QWERTY] = LAYOUT(
     //|-----------------------------------------------------|                    |-----------------------------------------------------|
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_ALT),
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,  TD(TD_PC), TD(TD_COMI),
     //---------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  TD(TD_SLA), TD(TD_APP_CAPLCK),
+    KC_LCTL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  TD(TD_SLA), TD(TD_APP_CAPLCK),
     //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LCTL, LOWER, KC_SPC,         KC_ENT, RAISE, TD(TD_ALT)
+                                        KC_LSFT, LOWER, KC_SPC,         KC_ENT, RAISE, KC_BSPC
                                         //|--------------------------|  |--------------------------|
     ),
 
     [L_LOWER] = LAYOUT(
     //|-----------------------------------------------------|                    |-----------------------------------------------------|
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_RALT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, KC_RGHT, KC_UP, KC_DOWN, KC_RGHT, LALT(KC_G),                     KC_HOME , KC_END, KC_PGUP, KC_PGDOWN, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, LALT(KC_G),                     KC_HOME , KC_END, KC_PGUP, KC_PGDOWN, XXXXXXX, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      DYN_REC_STOP , DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, DYN_REC_START1,  DYN_REC_START2, KC_LGUI,
+    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      DYN_REC_STOP , DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, DYN_REC_START1,  DYN_REC_START2, KC_LGUI,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LCTL, KC_TRNS, KC_SPC,       KC_ENT, RAISE, KC_RALT
+                                        KC_LSFT, KC_TRNS, KC_SPC,       KC_ENT, RAISE, KC_DEL
                                         //|--------------------------|  |--------------------------|
     ),
 
     [L_RAISE] = LAYOUT(
     //|-----------------------------------------------------|                    |-----------------------------------------------------|
-    KC_GRV,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+    KC_GRV,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RALT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_F1, KC_F2, KC_F3,   KC_F4, KC_F5, KC_F6,                                KC_LBRC, KC_RBRC, KC_BSLS,  XXXXXXX, XXXXXXX, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                               KC_MINS, KC_EQUAL, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI, LOWER, KC_LSFT,         KC_SPC, KC_TRNS, KC_RALT
+                                        KC_LGUI, LOWER, KC_LSFT,         KC_SPC, KC_TRNS, KC_BSPC
                                         //|--------------------------|  |--------------------------|
     ),
 
@@ -190,6 +190,7 @@ const char CodeToName[60] =
 };
 
 void OledRenderKeylog(void) { oled_write(keyLog, false); }
+
 void SetKeylog(uint16_t keycode, keyrecord_t* record) 
 {
     char name = ' ';
@@ -211,6 +212,11 @@ void SetKeylog(uint16_t keycode, keyrecord_t* record)
         record->event.key.col, 
         keycode, name
     );
+}
+
+void ClearKeylog(void)
+{
+    snprintf(keyLog, sizeof(keyLog), "\n");
 }
 
 void OledRenderLayerState(void)
@@ -243,7 +249,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation)
     return !is_master ? OLED_ROTATION_180 : rotation;
 }
 
-void oled_task_user(void) 
+void oled_task_user(void)
 {
     if (timer_elapsed32(oled_timer) > 1500000) 
     {
@@ -292,6 +298,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
 
     if(record->event.pressed)
         SetKeylog(keycode, record);
+    else
+        ClearKeylog();
 
     bool swallow = false;
     switch(keycode)
